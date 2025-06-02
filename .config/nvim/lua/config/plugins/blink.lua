277f0c025f93766b94294a130db6ec2544844e2e
Sarguru02 <psgnhealth@gmail.com>
2025-06-02 15:55:10 +0530

    [NVIM]: Update blink

--- .config/nvim/lua/config/plugins/blink.lua
+++ .config/nvim/lua/config/plugins/blink.lua
@@ -1,37 +1,56 @@
+local fn = vim.fn
+local opts = {
+	keymap = { preset = "default" },
+
+	appearance = {
+		nerd_font_variant = "mono",
+	},
+	completion = { documentation = { auto_show = true } },
+	sources = {
+		default = { "lsp", "path", "snippets", "buffer" },
+		providers = {
+			cmdline = {
+				name = "cmdline",
+				enabled = function()
+					return fn.has("win32") == 0
+						or fn.getcmdtype() ~= ":"
+						or not fn.getcmdline():match("^[%%0-9,'<>%-]*!")
+				end,
+				module = "blink.cmp.sources.cmdline",
+			},
+		},
+	},
+	cmdline = {
+		enabled = true,
+		completion = {
+			list = {
+				selection = {
+					preselect = true,
+					auto_insert = true,
+				},
+			},
+			menu = {
+				auto_show = true,
+			},
+			ghost_text = {
+				enabled = false,
+			},
+		},
+	},
+	fuzzy = { implementation = "lua" },
+}
+
 return {
-  'saghen/blink.cmp',
-  -- optional: provides snippets for the snippet source
-  dependencies = { 'rafamadriz/friendly-snippets' },
+	"saghen/blink.cmp",
+	-- optional: provides snippets for the snippet source
+	dependencies = { "rafamadriz/friendly-snippets" },
 
-  -- use a release tag to download pre-built binaries
-  version = '1.*',
+	-- use a release tag to download pre-built binaries
+	version = "1.*",
 
-  ---@module 'blink.cmp'
-  ---@type blink.cmp.Config
-  opts = {
-    -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-    -- 'super-tab' for mappings similar to vscode (tab to accept)
-    -- 'enter' for enter to accept
-    -- 'none' for no mappings
-    --
-    -- All presets have the following mappings:
-    -- C-space: Open menu or open docs if already open
-    -- C-n/C-p or Up/Down: Select next/previous item
-    -- C-e: Hide menu
-    -- C-k: Toggle signature help (if signature.enabled = true)
-    --
-    keymap = { preset = 'default' },
-
-    appearance = {
-      nerd_font_variant = 'mono'
-    },
-    completion = { documentation = { auto_show = true } },
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
-    },
-    fuzzy = { implementation = "prefer_rust_with_warning" }
-  },
-  build = 'cargo build --release',
-  lazy = true,
-  opts_extend = { "sources.default" }
+	---@module 'blink.cmp'
+	---@type blink.cmp.Config
+	opts = opts,
+	build = "cargo build --release",
+	lazy = true,
 }
