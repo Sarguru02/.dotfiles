01f2b1da6a15835b73dcb56be8167a5d8e99e59b
Sarguru02 <psgnhealth@gmail.com>
2025-07-18 13:42:30 +0530

    [NVIM]: Moving back to netrw🙃

--- .config/nvim/lua/config/maps.lua
+++ .config/nvim/lua/config/maps.lua
@@ -3,6 +3,8 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
+vim.keymap.set("n","<leader>pv", "<cmd>Ex<CR>")
+
 -- move lines
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
